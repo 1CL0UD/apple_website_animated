cf0947ed11b1163d8fa032f8bc27948a7f569dcd
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-06-15 23:52:56 +0700

    configured sentry

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
+
+import * as Sentry from '@sentry/react';
+
 const App = () => {
-  return (
-    <main className="bg-black">
-      <Navbar />
-      <Hero />
-      <Highlights />
-      <Model />
-    </main>
-  );
+  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+  // return (
+  //   <main className="bg-black">
+  //     <Navbar />
+  //     <Hero />
+  //     <Highlights />
+  //     <Model />
+  //   </main>
+  // );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
