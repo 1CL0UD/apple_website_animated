8b7655a024e3bad3339cb7408d6c068cb3d487cf
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2024-06-16 00:00:02 +0700

    Revert "configured sentry"
    This reverts commit cf0947ed11b1163d8fa032f8bc27948a7f569dcd.

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,15 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
-
-import * as Sentry from '@sentry/react';
-
 const App = () => {
-  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
-  // return (
-  //   <main className="bg-black">
-  //     <Navbar />
-  //     <Hero />
-  //     <Highlights />
-  //     <Model />
-  //   </main>
-  // );
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
+  );
 };
 
-export default Sentry.withProfiler(App);
+export default App;
