fbd71f54b10c914e4ab896267c4f64453cb16fe2
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-06-15 19:06:39 +0700

    made navbar 50%

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
+import { appleImg, bagImg, searchImg } from '../utils/index';
+import { navList } from '../constants/index';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+      </nav>
+      <div className="flex flex-1 justify-center max-sm:hidden">
+        {navList.map((item) => (
+          <div key={item}>{item}</div>
+        ))}
+      </div>
+      <div>
+        <img src={searchImg} alt="Search" width={18} height={18} />
+        <img src={bagImg} alt="Bag" width={18} height={18} />
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
